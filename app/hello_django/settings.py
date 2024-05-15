c2b20a08edb782713b69705d3de96147741edca1
Nikhila <marripatinikhila@gmail.com>
2024-05-14 23:19:33 -0700

    fixes

--- app/hello_django/settings.py
+++ app/hello_django/settings.py
@@ -125,4 +125,4 @@ STATIC_URL = '/static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
-D
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
